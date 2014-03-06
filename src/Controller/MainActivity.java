f5129debeb6b210cd0acd8838c8fdaf29ad13377
NubCoda <al.khatib.sami1990@googlemail.com>
2014-03-06 11:26:58 +0100

    Mainactivity added

---
+++ src/Controller/MainActivity.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import View.MainFrame;
+
+public class MainActivity {
+
+	public static void main(String[] args) {
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.setVisible(true);
+	}
+
+}
