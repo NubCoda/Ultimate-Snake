3a4e32c62f9f8c555d36f5838db9187e827f4f6c
NubCoda <al.khatib.sami1990@googlemail.com>
2014-03-06 11:05:15 +0100

    Keylistener

---
+++ src/View/KeyListener.java
@@ -0,0 +1,26 @@
+package View;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyListener extends KeyAdapter {
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+			case 39:	// -> Right
+				break;
+			case 38:	// -> Top
+				break;
+				
+			case 37: 	// -> Left 
+				break;
+				
+			case 40:	// -> Bottom
+				break;
+		default:
+			break;
+		}
+	}
+	
+}
