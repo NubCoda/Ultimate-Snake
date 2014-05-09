526d81753ffcecdd4486f0fac1f6271fa3473053
NubCoda <al.khatib.sami1990@googlemail.com>
2014-05-09 10:34:42 +0200

    Singelton

--- src/Controller/OptionsController.java
+++ src/Controller/OptionsController.java
@@ -1,5 +1,16 @@
 package Controller;
 
 public class OptionsController {
-
+	private static OptionsController optionsController;
+	
+	private OptionsController() {
+		
+	}
+	
+	public static OptionsController getInstance() {
+		if(optionsController == null) {
+			optionsController = new OptionsController();
+		}
+		return optionsController;
+	}
 }
