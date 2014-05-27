db40a791a6297ac5fb532273e46cf880a3a5b4dc
NubCoda <al.khatib.sami1990@googlemail.com>
2014-05-27 15:26:58 +0200

    Removed Files and shit

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -1,11 +1,8 @@
 package View;
 
-import java.awt.Dimension;
 import java.awt.DisplayMode;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.Toolkit;
-import java.awt.event.FocusAdapter;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -26,16 +23,13 @@ public class MainView extends JFrame implements Observer {
 	public MainView(GamePanelView gamePanelView) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		add(gamePanelView);
-//		setDefaultLookAndFeelDecorated(true);
+//		setSize(800, 600);
 		setUndecorated(true);
 		setResizable(false);
-		setVisible(true);
 		GraphicsEnvironment env = GraphicsEnvironment
 				.getLocalGraphicsEnvironment();
 		GraphicsDevice device = env.getDefaultScreenDevice();
-//		device.setFullScreenWindow(this);
-		setSize(device.getDisplayMode().getWidth(), device.getDisplayMode().getHeight());
-//		device.setDisplayMode(new DisplayMode(device.getDisplayMode().getWidth(), device.getDisplayMode().getHeight(), device.getDisplayMode().getBitDepth(), device.getDisplayMode().getRefreshRate()));
+		device.setFullScreenWindow(this);
 	}
 
 	@Override
