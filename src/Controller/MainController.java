7b401505a6178caeb58f9fa9d58dad5ff9dc5ba1
NubCoda <al.khatib.sami1990@googlemail.com>
2014-05-31 11:10:50 +0200

    raise milliseconds

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -304,7 +304,7 @@ public class MainController {
 			@Override
 			public void run() {
 				try {
-					Thread.sleep(500);
+					Thread.sleep(650);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
